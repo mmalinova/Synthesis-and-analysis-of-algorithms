7d0df1e39c4f97621e1a9a953894fb271aa1450c
Mihaela Malinova <malinova29@gmail.com>
2020-08-24 12:52:40 +0300

    Add files via upload

---
+++ Exercise12/Ex1/Ex1.cpp
@@ -0,0 +1,120 @@
+﻿// Ex1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "stdio.h"
+
+void quicksort(float A[], int start, int stop)
+{
+	int i = start, j = stop;
+	float m, x;
+	m = A[(start + stop) / 2];
+	do {
+		while (A[i] < m) i++;
+		while (A[j] > m) j--;
+		if (i <= j)
+		{
+			x = A[i];
+			A[i] = A[j];
+			A[j] = x;
+			i++;
+			j--;
+		}
+	} while (i <= j);
+	if (start < j)
+		quicksort(A, start, j);
+	if (i < stop)
+		quicksort(A, i, stop);
+}
+
+void printArray(float A[], int size)
+{
+	for (int i = 0; i < size; i++)
+		printf("%.1f\n", A[i]);
+}
+
+int BinSearch(int l, int r, float x, float A[])
+{
+	int m;
+	while (l <= r)
+	{
+		m = (l + r) / 2;
+		if (x < A[m])
+			r = m - 1;
+		else
+			if (x > A[m])
+				l = m + 1;
+			else
+				return m;
+	}
+	return -1;
+}
+
+int SecondSearch(int n, float B[], float x)
+{
+	int i = 0;
+	B[n] = x;
+	while (B[i] != x)
+		i++;
+	if (i < n)
+		return i;
+	return -1;
+}
+
+int FirstSearch(int n, float B[], float x)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		if (B[i] == x)
+			return i;
+	return -1;
+}
+
+int main()
+{
+	int n, i, k;
+	float x;
+	printf("Enter number of elements: ");
+	scanf_s("%d", &n);
+	float *A = (float*)malloc(sizeof(float) * n);
+	printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("Enter element for A[%d]: ", i);
+		scanf_s("%f", &A[i]);
+	}
+	printf("\nEnter search element: ");
+	scanf_s("%f", &x);
+
+	quicksort(A, 0, n - 1);
+	printf("\nSorted array is: \n");
+	printArray(A, n);
+	k = BinSearch(0, n - 1, x, A);
+	if (k == -1)
+		printf("\nThe element is not in the array!\n\n");
+	else
+		printf("\nThe element is on %d position!\n\n", k);
+
+	float *B = (float*)malloc(sizeof(float) * (n + 1));
+	for (i = 0; i < n; i++)
+	{
+		printf("Enter element for B[%d]: ", i);
+		scanf_s("%f", &B[i]);
+	}
+	printf("\nEnter search element: ");
+	scanf_s("%f", &x);
+
+	k = SecondSearch(n, B, x);
+	if (k == -1)
+		printf("\nThe element is not in the original array!\n");
+	else
+		printf("\nThe element is on %d position!\n", k);
+
+	k = FirstSearch(n, B, x);
+	if (k == -1)
+		printf("\nThe element is not in the array!\n");
+	else
+		printf("\nThe element is on %d position!\n", k);
+}
+
---
+++ Exercise12/Ex2/Ex2.cpp
@@ -0,0 +1,163 @@
+﻿// Ex2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "stdio.h"
+
+struct tree
+{
+	char key;
+	struct tree *left;
+	struct tree *right;
+};
+
+struct tree * SearchTree(char x, struct tree *v)
+{
+	if (v != NULL)
+	{
+		if (x < v->key)
+			SearchTree(x, v->left);
+		else
+			if (x > v->key)
+				SearchTree(x, v->right);
+			else
+				return v;
+		printf("The element is found: %c!", v);
+	}
+	else
+		printf("The element is not in the tree!");
+		return NULL; 
+}
+
+void InsertNode(char x, struct tree ** v)
+{ 
+	if (*v != NULL)
+	{
+		if (x < (*v)->key)
+			InsertNode(x, &(*v)->left);
+		else   
+			if (x > (*v)->key) 
+				InsertNode(x, &(*v)->right);
+	}
+	else
+	{ 
+		(*v) = (struct tree *) malloc(sizeof(struct tree));
+		(*v)->key = x;  
+		(*v)->left = NULL;
+		(*v)->right = NULL; 
+	}
+}
+
+void DeleteNode(char x, struct tree ** v)
+{
+	if (*v == NULL)
+		printf("\nThe element miss!");
+	else
+	{
+		if (x < (*v)->key)
+			DeleteNode(x, &(*v)->left);
+		else
+			if (x > (*v)->key)
+				DeleteNode(x, &(*v)->right);
+			else
+			{
+				if ((*v)->left != NULL && (*v)->right != NULL)
+				{
+					struct tree * h = (*v)->right;
+					while (h->left != NULL)
+						h = h->left;
+					(*v)->key = h->key;
+					DeleteNode((*v)->key, &(*v)->right);
+				}
+				else
+				{
+					struct tree * tmp = *v;
+					if ((*v)->left)
+						(*v) = (*v)->left;
+					else
+						(*v) = (*v)->right;
+					free(tmp);
+				}
+			}
+	}
+}
+
+void readtree(struct tree **v, char &x)
+{
+	char y, z;
+	struct tree * q1, *q2;
+	if (*v == NULL)
+	{
+		printf("Enter the root: ");
+		scanf_s("%c", &x);
+		(*v) = (struct tree *)malloc(sizeof(struct tree));
+		(*v)->key = x;
+	}
+	fflush(stdin);
+	printf("\nEnter left heir on %c: ", x);
+	z = getchar();
+	fflush(stdin);
+	printf("\nEnter right heir on %c: ", x);
+	y = getchar();
+	if (z != '0')
+	{
+		q1 = (struct tree *)malloc(sizeof(struct tree));
+		q1->key = z;
+		(*v)->left = q1;
+		readtree(&q1, z);
+	}
+	else
+		(*v)->left = NULL;
+	if (y != '0')
+	{
+		q2 = (struct tree *)malloc(sizeof(struct tree));
+		q2->key = y;
+		(*v)->right = q2;
+		readtree(&q2, y);
+	}
+	else
+		(*v)->right = NULL;
+}
+
+void printtree(struct tree * v, int delta)
+{
+	int i;
+	if (v != NULL)
+	{
+		printtree(v->left, delta + 1);
+		for (i = 1; i <= delta; i++)
+			printf("\t");
+		printf("%c", v->key);
+		printf("\n");
+		printtree(v->right, delta + 1);
+	}
+}
+
+int main()
+{
+	struct tree *tree;
+	tree = NULL;
+	char key;
+	char element_to_search, element_to_add, element_to_delete;
+
+	readtree(&tree, key);
+	printf("\n");
+	printtree(tree, 0);
+	printf("\n");
+
+	printf("Enter element to search: ");
+	scanf_s("%c", &element_to_search);
+	SearchTree(element_to_search, tree);
+	printf("Enter element to add: ");
+	scanf_s("%c", &element_to_add);
+	InsertNode(element_to_add, &tree);
+	printf("Enter element to delete: ");
+	scanf_s("%c", &element_to_delete);
+	DeleteNode(element_to_delete, &tree);
+
+	printf("The new tree is: ");
+	printtree(tree, 0);
+	printf("\n");
+	return 0;
+}
